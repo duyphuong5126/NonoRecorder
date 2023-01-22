0fad6a21fa82feb3c942c9161a92c5028af19449
duyphuong5126 <duyphuong5126@gmail.com>
2023-01-22 16:37:16 +0700

    Don't set relative path on android 9 and below

--- app/src/main/java/com/nonoka/nonorecorder/shared/ContextExt.kt
+++ app/src/main/java/com/nonoka/nonorecorder/shared/ContextExt.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.os.Build.VERSION.SDK_INT
 import android.os.Build.VERSION_CODES.Q
 import android.os.Build.VERSION_CODES.S
-import android.os.Environment
 import android.os.Environment.DIRECTORY_MUSIC
 import android.os.Environment.DIRECTORY_RECORDINGS
 import android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
@@ -20,41 +19,25 @@ import timber.log.Timber
 
 @Throws(Throwable::class)
 fun Context.createSharedAudioFile(mp3File: File) {
-    if (SDK_INT < Q) {
-        try {
-            val directory = Environment.getExternalStoragePublicDirectory(DIRECTORY_MUSIC)
-            val file = File(directory, "$externalRecordedFolder/${mp3File.name}").apply {
-                createNewFile()
-            }
-            file.outputStream().let { outputStream ->
-                writeFileToOutputStream(mp3File, outputStream)
-                Timber.d("Recording>>> Saved file ${mp3File.name} into ${file.path}")
-            }
-        } catch (error: Throwable) {
-            Timber.d("Recording>>> Could not save file ${mp3File.name} into music folder with error $error")
-            throw error
-        }
-    } else {
-        val contentValues = ContentValues()
-        contentValues.put(DISPLAY_NAME, mp3File.name)
-        contentValues.put(MIME_TYPE, "audio/mpeg")
-        if (SDK_INT >= S) {
-            contentValues.put(RELATIVE_PATH, "$DIRECTORY_RECORDINGS/$externalRecordedFolder")
-        } else {
-            contentValues.put(RELATIVE_PATH, "$DIRECTORY_MUSIC/$externalRecordedFolder")
-        }
+    val contentValues = ContentValues()
+    contentValues.put(DISPLAY_NAME, mp3File.name)
+    contentValues.put(MIME_TYPE, "audio/mpeg")
+    if (SDK_INT >= S) {
+        contentValues.put(RELATIVE_PATH, "$DIRECTORY_RECORDINGS/$externalRecordedFolder")
+    } else if (SDK_INT >= Q) {
+        contentValues.put(RELATIVE_PATH, "$DIRECTORY_MUSIC/$externalRecordedFolder")
+    }
 
-        try {
-            contentResolver.insert(EXTERNAL_CONTENT_URI, contentValues)?.let { uri ->
-                contentResolver.openOutputStream(uri)?.let { outputStream ->
-                    writeFileToOutputStream(mp3File, outputStream)
-                    Timber.d("Recording>>> Saved file ${mp3File.name} into $uri")
-                }
+    try {
+        contentResolver.insert(EXTERNAL_CONTENT_URI, contentValues)?.let { uri ->
+            contentResolver.openOutputStream(uri)?.let { outputStream ->
+                writeFileToOutputStream(mp3File, outputStream)
+                Timber.d("Recording>>> Saved file ${mp3File.name} into $uri")
             }
-        } catch (error: Throwable) {
-            Timber.d("Recording>>> Could not insert file ${mp3File.name} to media store with error $error")
-            throw error
         }
+    } catch (error: Throwable) {
+        Timber.d("Recording>>> Could not insert file ${mp3File.name} to media store with error $error")
+        throw error
     }
 }
 
